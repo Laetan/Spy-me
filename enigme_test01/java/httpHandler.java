50f60a04410f63d91628cca307cb085c1488dfab
schaltiemi <aurore.bonnin@hotmail.fr>
2014-02-14 12:20:31 +0100

    enigme_test01
    Enigme.
    Requete au serveur pour Enigme ET réponse
    Problème d'accents dans la réponse du serveur ET dans l'editText

---
+++ enigme_test01/java/httpHandler.java
@@ -0,0 +1,31 @@
+package com.example.enigme;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+
+public class httpHandler {
+	
+	 public String post(String posturl){
+			try {
+				
+				HttpClient htppclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(posturl);
+				
+				HttpResponse resp = htppclient.execute(httppost);
+				HttpEntity ent = resp.getEntity();
+				
+				String text = EntityUtils.toString(ent);
+				
+				return text;
+				
+			}
+			catch(Exception e){return "error";}
+
+}
+	 
+}
