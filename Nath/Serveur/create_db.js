310ab0ff36c6380e20a50ad8c4bb296731c3ab48
Laetan <nathanael.julien@icloud.com>
2014-03-01 16:16:32 +0100

    implémentation clé de la bdd

--- Nath/Serveur/create_db.js
+++ Nath/Serveur/create_db.js
@@ -1,5 +1,3 @@
-//Fichier de création de la base de donnée. N'est pas appelée par le serveur.
-
 var fs=require('fs');
 var file="spyme.db";
 var exists=fs.existsSync(file);
@@ -8,9 +6,9 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
 
  db.serialize(function(){
-	 db.run("CREATE TABLE JOUEUR (pseudo TEXT,password TEXT, niveau_joueur INTEGER, points INTEGER)");
-	 db.run("CREATE TABLE ENIGMES (id INTEGER, path TEXT, niveau_enigme INTEGER)");
-	 db.run("CREATE TABLE JOUEUR_ENIGMES (pseudo TEXT, enigme INTEGER, est_resolue INTEGER)");
+	 db.run("CREATE TABLE JOUEUR (pseudo TEXT PRIMARY KEY,password TEXT, niveau_joueur INTEGER, points INTEGER)");
+	 db.run("CREATE TABLE ENIGMES (id INTEGER PRIMARY KEY, path TEXT, niveau_enigme INTEGER)");
+	 db.run("CREATE TABLE JOUEUR_ENIGMES (pseudo TEXT REFERENCES JOUEUR(pseudo), enigme INTEGER, est_resolue INTEGER, FOREIGN KEY(pseudo) REFERENCES JOUEUR(pseudo),FOREIGN KEY(enigme) REFERENCES ENIGMES(id))");//
 
 	 db.run("INSERT INTO ENIGMES VALUES (1,'Enigmes/enigme01', 0)");
  });
